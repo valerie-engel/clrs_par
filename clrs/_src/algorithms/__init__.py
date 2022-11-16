15e835090c7a4373cb9b826b7301a98ab0439d1e
valerie-engel <90622795+valerie-engel@users.noreply.github.com>
2022-11-16 14:11:04 +0100

    Update __init__.py
    add parallel_search and odd_even_transp_sort

--- clrs/_src/algorithms/__init__.py
+++ clrs/_src/algorithms/__init__.py
@@ -46,9 +46,11 @@ from clrs._src.algorithms.greedy import activity_selector
 from clrs._src.algorithms.greedy import task_scheduling
 
 from clrs._src.algorithms.searching import minimum
+from clrs._src.algorithms.searching import parallel_search
 from clrs._src.algorithms.searching import binary_search
 from clrs._src.algorithms.searching import quickselect
 
+from clrs._src.algorithms.sorting import odd_even_transp_sort
 from clrs._src.algorithms.sorting import insertion_sort
 from clrs._src.algorithms.sorting import bubble_sort
 from clrs._src.algorithms.sorting import heapsort
